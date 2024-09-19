2189ef321d4da1669ecf50517893852b92951f37
JAG24_Exo <joseguillen587@yahoo.com>
2024-09-19 16:12:29 -0700

    added simple fetch test of 5 random poems

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@
      * You do NOT need update, but you can add it if you'd like
      * Use Bootstrap and/or CSS to style your project
 */
+
+async function fetchPoems() {
+    try {
+        const response = await fetch('https://poetrydb.org/random/5');
+        const poems = await response.json();
+        
+        // Console log the fetched poems
+        console.log(poems);
+
+        // Display the poems (assuming you have a function for rendering them)
+        // displayPoems(poems);
+    } catch (error) {
+        console.error('Error fetching poems:', error);
+    }
+}
+
+fetchPoems();
+
