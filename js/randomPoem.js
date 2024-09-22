094ed9e8dbc0125defb1f11607af2143c546e8b3
JAG24_Exo <joseguillen587@yahoo.com>
2024-09-21 22:12:50 -0700

    changed bootstrap properties on generated random poem

--- js/randomPoem.js
+++ js/randomPoem.js
@@ -24,16 +24,16 @@ function initializeRandomPoemGenerator() {
                 // Add HTML to the page
                 const poemCardRandomHTML = `
                     <div class="row justify-content-center">
-                        <div class="col-md-6 col-lg-4 mb-4">
+                        <div class="col-md-8 mb-4">
                             <div class="card">
-                                <div class="card-body p-2">
+                                <div class="card-body p-4">
                                     <h5 class="card-title">${poem.title}</h5>
                                     <h6 class="card-subtitle mb-2 text-muted">${poem.author}</h6>
                                     <p class="card-text">Line Count: ${poem.linecount}</p>
                                     <p class="card-text">
                                         ${poem.lines.join('<br>')}
                                     </p>
-                                    <button class="btn btn-danger btn-sm position-absolute top-0 end-0 m-2 delete-poem">
+                                    <button class="btn btn-danger btn-lg-1 position-absolute top-0 end-0 m-2 delete-poem">
                                         <i class="bi bi-trash"></i>
                                     </button>
                                 </div>
