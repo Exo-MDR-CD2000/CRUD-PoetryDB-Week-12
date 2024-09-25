be56b22c2ab80b1ccec157554eba594d847720cd
JAG24_Exo <joseguillen587@yahoo.com>
2024-09-25 14:55:55 -0700

    template code for new js file

--- js/createNewPoem.js
+++ js/createNewPoem.js
@@ -1,1 +1,25 @@
-// JS code to handle new poems creation and save them to the database
+// JS code to handle the creation of a new poem and save them to the db
+// Can probably import code from searchPoem.js like the handleSavePoem fx and displaySavedPoems
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const addPoemForm = document.getElementById('addPoemForm'); // selects the form element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
+
+
+// remember this cmd line to run the server: json-server --watch db/db.json
